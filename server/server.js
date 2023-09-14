8498c4aed4aa0148b660008672447bbaa70a6ce6
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-09-13 23:32:17 -0600

    push number eight

---
+++ server/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+// Configure CORS, bodyParser, and other middleware as needed
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+// Create a Nodemailer transporter
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // Use TLS
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+
+// Define a POST endpoint for handling form submissions
+app.post('/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: 'New Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email: ', error);
+      res.status(500).json({ success: false, message: 'Failed to send email' });
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).json({ success: true, message: 'Email sent successfully' });
+    }
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3003;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
