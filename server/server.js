1fb7c1869e6baaf897d8efda4fd213bf1b5d6011
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-09-14 19:16:24 -0600

    following right path

--- server/server.js
+++ server/server.js
@@ -56,10 +56,10 @@ app.post('/send-email', (req, res) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '..client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 // Start the server
